151d0f9ab08becb9dd3c290e24f73bfc95f743b6
Castlecelts <jamesdrmundie@gmail.com>
2018-10-19 13:37:46 +0100

    define setup of problem

--- start_point/pet_shop.rb
+++ start_point/pet_shop.rb
@@ -1,1 +1,9 @@
-kjhkjhkjh
+
+#The setup.
+#Customers[array] -> subset of {hashes}
+#new_pet{hash}
+#pet_shop{2hashs} ->pets[array] -> subset of {hashes}
+#                 ->admin{hashes}
+#                 ->name
+
+###########################################################
