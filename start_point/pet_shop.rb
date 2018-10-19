d83ca3bea1c55254e550cd9e28a58cd9bb18c677
Castlecelts <jamesdrmundie@gmail.com>
2018-10-19 13:57:09 +0100

    solution to test 2 and set up of test 3

--- start_point/pet_shop.rb
+++ start_point/pet_shop.rb
@@ -15,3 +15,16 @@
 def pet_shop_name(shops)
  return shops[:name]
 end
+
+#calc total cash
+#method = total_cash
+#function should return the value of total_cash in pet_shop
+
+def total_cash(shops)
+  return shops[:admin][:total_cash]
+end
+
+#calc total_cash after adding cash
+#method new add_or_remove_cash
+#method also called total_cash
+#function should take an input and add it to total_cash
