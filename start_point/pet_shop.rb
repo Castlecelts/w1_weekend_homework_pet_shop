00859d3d33f8951f487afc888a202d675014b776
Castlecelts <jamesdrmundie@gmail.com>
2018-10-19 15:08:25 +0100

    test 9 solution, note in code

--- start_point/pet_shop.rb
+++ start_point/pet_shop.rb
@@ -8,7 +8,7 @@
 
 ###########################################################
 
-#Pet shop name function
+#test 1 - Pet shop name function
 #method = pet_shop_name
 #function should - return the name of the pet_shop
 
@@ -16,7 +16,7 @@ def pet_shop_name(shops)
  return shops[:name]
 end
 
-#calc total cash
+#test 2 - calc total cash
 #method = total_cash
 #function should return the value of total_cash in pet_shop
 
@@ -24,7 +24,7 @@ def total_cash(shops)
   return shops[:admin][:total_cash]
 end
 
-#calc total_cash after adding cash
+#test 3 - calc total_cash after adding cash
 #method new add_or_remove_cash
 #method also called total_cash
 #function should take an input and add it to total_cash
@@ -34,13 +34,13 @@ def add_or_remove_cash(shops, value)
   return shops[:admin][:total_cash] += sum_to_add
 end
 
-#calc total_cash after subtracting cash
+#test 4 - calc total_cash after subtracting cash
 #method called add_or_remove_cash
 #method called total_cash
 #function should now also subtract from total_cash
 #horray - method add_or_remove_cash already does this!!
 
-#calc number of number of pets_sold
+#test 5 - calc number of number of pets_sold
 #method = pets_sold
 #function should return the value of pets_sold in admin
 
@@ -48,7 +48,7 @@ def pets_sold(shops)
   return shops[:admin][:pets_sold]
 end
 
-#increase the number of pets sold
+#test 6 - increase the number of pets sold
 #method increase_pets_sold
 #method also called pets_sold
 #function should increase the value of pets_sold in admin
@@ -58,7 +58,7 @@ def increase_pets_sold(shops, numsoldpets)
   return shops[:admin][:pets_sold] += num_pets_sold
 end
 
-#calc number of pets in the pet_shop
+#test 7 - calc number of pets in the pet_shop
 #method = stock_count
 #function should take the size of pets. (maybe does not need to return??)
 
@@ -66,7 +66,7 @@ def stock_count(shops)
   shops[:pets].length
 end
 
-#calc how many of each type of breed is in the pet_shop
+#test 8 - calc how many of each type of breed is in the pet_shop
 #method = pets_by_breed
 #function find the total of a specific breed of pet_type
 
@@ -75,6 +75,14 @@ def pets_by_breed(shops, breed)
   for x in shops[:pets]
     if x[:breed] == breed
       return number_of_pets_by_breed += number_of_pets_by_breed.push(x)
+    else
+      return number_of_pets_by_breed
     end
   end
 end
+
+#test 9 - this should check if a breed is in stock, if it isn't then 0 should be returned.
+#method called pets_by_breed
+#function should give a 0 count if breed not found
+
+#NOTE the else with return line is ESSENTIAL. otherwise the function does not return anything and instead shows all dogs.
